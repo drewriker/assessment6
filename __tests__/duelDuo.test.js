dcdeaa2faed994e55e25c216d60fe8da0d320b5a
Drew Riker <drewriker@gmail.com>
2024-04-02 20:06:02 -0600

    duelDuo tests added

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -3,16 +3,41 @@ const { Builder, Browser, By, until } = require("selenium-webdriver");
 let driver;
 
 beforeEach(async () => {
-  driver = await new Builder().forBrowser(Browser.CHROME).build();
+	driver = await new Builder().forBrowser(Browser.CHROME).build();
 });
 
 afterEach(async () => {
-  await driver.quit();
+	await driver.quit();
 });
 
 describe("Duel Duo tests", () => {
-  test("page loads with title", async () => {
-    await driver.get("http://localhost:8000");
-    await driver.wait(until.titleIs("Duel Duo"), 1000);
-  });
+	test("page loads with title", async () => {
+		await driver.get("http://localhost:8000");
+		await driver.wait(until.titleIs("Duel Duo"), 1000);
+	});
+
+	test("draw button displays choices", async () => {
+		await driver.get("http://localhost:8000");
+		await driver.findElement(By.id("draw")).click();
+
+		const displayDiv = await driver.wait(
+			until.elementLocated(By.id('choices')),
+			1000
+		);
+
+    expect(await displayDiv).toBeTruthy()
+	});
+
+  test("draw button displays choices", async () => {
+		await driver.get("http://localhost:8000");
+		await driver.findElement(By.id("draw")).click();
+    await driver.findElement(By.className("bot-btn")).click();
+
+		const displayDiv = await driver.wait(
+			until.elementLocated(By.id('player-duo')),
+			1000
+		);
+
+    expect(await displayDiv).toBeTruthy()
+	});
 });
